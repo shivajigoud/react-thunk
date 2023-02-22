ac887660a6c65237a3262fbe3ca1db2754073cb8
shivajigoud <shiva.vumma@gmail.com>
2023-02-22 19:37:43 +0530

    1 file modified

--- src/actions/todoAction.js
+++ src/actions/todoAction.js
@@ -2,31 +2,26 @@ import { ADD_TODOS, UPDATE_TODOS, DELETE_TODOS } from './actions.js';
 
 export const addTodo = (payload) => async (dispatch, action) => {
   const currentTodos = await getTodos();
-  const newTodos = await currentTodos.then(
-    (todso) => ({
-      ...currentTodos,
-      payload,
-    }),
-    (error) => {
-      console.log(error);
-    }
-  );
-  await addTodos(newTodos);
+  const todos = JSON.parse(currentTodos);
+  await addTodos({ ...todos, payload });
   dispatch(ADD_TODOS, payload);
 };
 
 function getTodos() {
   return new Promise((resolve, reject) => {
     const todos = localStorage.getItem('Todos');
-    if (todos && todos.json().length > 0) {
+    if (todos) {
       resolve(todos);
-    } else reject('No todos stored yet');
+    } else {
+      localStorage.setItem('Todos', JSON.stringify([]));
+      reject('No todos stored yet');
+    }
   });
 }
 function addTodos(todos) {
   return new Promise((resolve, reject) => {
     try {
-      localStorage.setItem(JSON.stringify(todos));
+      localStorage.setItem('Todos', JSON.stringify(todos));
       resolve('todos added to the local storage');
     } catch {
       reject('Problem in adding todos');
